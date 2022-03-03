eed71accadecf5920776d7693ae6b4444bd1214f
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-03 23:04:27 +0530

    updated views and created docker compose yaml

--- firstApp/views.py
+++ firstApp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def first_view(request):
-    return HttpResponse("My first docker django project.")
+    return HttpResponse("My first docker django project with Docker Compose and Volumes")
