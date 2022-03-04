fbe9a9a273401f2882d94605107e0d3c7516c7ed
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-04 18:05:54 +0530

    modified message in views

--- firstApp/views.py
+++ firstApp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def first_view(request):
-    return HttpResponse("My first docker django project with Docker Compose and Volumes")
+    return HttpResponse("I am the creator of my destiny")
